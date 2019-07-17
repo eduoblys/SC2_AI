883707841e0f3a0928a324db1a2911004f74c322
eduoblys <eduoblys@gmail.com>
2019-07-18 00:48:44 +0200

    pt3. gas

--- src/test1.py
+++ src/test1.py
@@ -1,19 +1,23 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 
 class SentdeBot(sc2.BotAI):
     async def on_step(self, iteration):
-        # what to do every step
         await self.distribute_workers()  # in sc2/bot_ai.py
-        await self.build_workers()
-        await self.build_pylons()
-
+        await self.build_workers()  # workers bc obviously
+        await self.build_pylons()  # pylons are protoss supply buildings
+        await self.expand()  # expand to a new resource area.
+        await self.build_assimilator()  # getting gas
 
     async def build_workers(self):
+        # nexus = command center
         for nexus in self.units(NEXUS).ready.noqueue:
+            # we want at least 20 workers, otherwise let's allocate 70% of our supply to workers.
+            # later we should use some sort of regression algo maybe for this?
+
             if self.can_afford(PROBE):
                 await self.do(nexus.train(PROBE))
 
@@ -24,7 +28,24 @@ class SentdeBot(sc2.BotAI):
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near=nexuses.first)
 
+    async def expand(self):
+        if self.units(NEXUS).amount < 2 and self.can_afford(NEXUS):
+            await self.expand_now()
+
+    async def build_assimilator(self):
+        for nexus in self.units(NEXUS).ready:
+            vaspenes = self.state.vespene_geyser.closer_than(25.0, nexus)
+            for vaspene in vaspenes:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vaspene.position)
+                if worker is None:
+                    break
+                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vaspene))
+
+
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, SentdeBot()),
     Computer(Race.Terran, Difficulty.Easy)
-], realtime=True)
+], realtime=False)
