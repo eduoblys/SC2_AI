41315122ccf0a682a25c638127a7e0d70c11074b
eduoblys <eduoblys@gmail.com>
2019-07-23 19:39:15 +0200

    building units

--- src/test1.py
+++ src/test1.py
@@ -1,23 +1,22 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
+ CYBERNETICSCORE, STALKER
 
 
 class SentdeBot(sc2.BotAI):
     async def on_step(self, iteration):
-        await self.distribute_workers()  # in sc2/bot_ai.py
-        await self.build_workers()  # workers bc obviously
-        await self.build_pylons()  # pylons are protoss supply buildings
-        await self.expand()  # expand to a new resource area.
-        await self.build_assimilator()  # getting gas
+        await self.distribute_workers()
+        await self.build_workers()
+        await self.build_pylons()
+        await self.build_assimilators()
+        await self.expand()
+        await self.offensive_force_buildings()
+        await self.build_offensive_force()
 
     async def build_workers(self):
-        # nexus = command center
         for nexus in self.units(NEXUS).ready.noqueue:
-            # we want at least 20 workers, otherwise let's allocate 70% of our supply to workers.
-            # later we should use some sort of regression algo maybe for this?
-
             if self.can_afford(PROBE):
                 await self.do(nexus.train(PROBE))
 
@@ -28,13 +27,9 @@ class SentdeBot(sc2.BotAI):
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near=nexuses.first)
 
-    async def expand(self):
-        if self.units(NEXUS).amount < 2 and self.can_afford(NEXUS):
-            await self.expand_now()
-
-    async def build_assimilator(self):
+    async def build_assimilators(self):
         for nexus in self.units(NEXUS).ready:
-            vaspenes = self.state.vespene_geyser.closer_than(25.0, nexus)
+            vaspenes = self.state.vespene_geyser.closer_than(15.0, nexus)
             for vaspene in vaspenes:
                 if not self.can_afford(ASSIMILATOR):
                     break
@@ -44,8 +39,28 @@ class SentdeBot(sc2.BotAI):
                 if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
                     await self.do(worker.build(ASSIMILATOR, vaspene))
 
+    async def expand(self):
+        if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+            await self.expand_now()
+
+    async def offensive_force_buildings(self):
+        if self.units(PYLON).ready.exists:
+            pylon = self.units(PYLON).ready.random
+            if self.units(GATEWAY).ready.exists:
+                if not self.units(CYBERNETICSCORE):
+                    if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
+                        await self.build(CYBERNETICSCORE, near=pylon)
+            else:
+                if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
+                    await self.build(GATEWAY, near=pylon)
+
+    async def build_offensive_force(self):
+        for gw in self.units(GATEWAY).ready.noqueue:
+            if self.can_afford(STALKER) and self.supply_left > 0:
+                await self.do(gw.train(STALKER))
+
 
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, SentdeBot()),
     Computer(Race.Terran, Difficulty.Easy)
-], realtime=False)
+    ], realtime=False)
